fb83abd8fc9677b53631b33926c53811a75f8564
Tiny Brick Boy <102919798+TinyBrickBoy@users.noreply.github.com>
2025-06-27 11:06:02 +0200

    Update joinnotifier.go

--- pkg/plugins/joinnotifier/joinnotifier.go
+++ pkg/plugins/joinnotifier/joinnotifier.go
@@ -1,4 +1,24 @@
 package joinnotifier
 
-// Temporär leeres Plugin - verhindert Compilation-Fehler
-// TODO: Implementiere das ursprüngliche Plugin falls nötig
+import (
+	"context"
+	"github.com/go-logr/logr"
+	"go.minekube.com/gate/pkg/edition/java/proxy"
+)
+
+// Plugin ist die Variable die von gate.go erwartet wird
+var Plugin = proxy.Plugin{
+	Name: "JoinNotifier",
+	Init: func(ctx context.Context, p *proxy.Proxy) error {
+		log := logr.FromContextOrDiscard(ctx)
+		log.Info("JoinNotifier Plugin loaded (placeholder)")
+		
+		// Hier könntest du später echte Join-Notification-Logik hinzufügen
+		// Zum Beispiel:
+		event.Subscribe(p.Event(), 0, func(e *proxy.PostLoginEvent) {
+	     log.Info("Player joined", "player", e.Player().Username())
+		})
+		
+		return nil
+	},
+}
